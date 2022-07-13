3b4247987c2effb85ddf7872eeef3b6a75fe5c99
Timchenko <tbd2504@gmail.com>
2022-07-13 19:56:54 +0300

    add file

---
+++ bank_account_app.py
@@ -0,0 +1,29 @@
+"""
+Описати клас Банківсткий рахунокб атрибути якого:
+- ім'я олікового запису - str
+- унікальний id - uuid
+- баланс - float or Decimal
+Методи
+    депозит коштів
+    виведення коштів
+    отримання балансу
+"""
+
+from decimal import Decimal
+from uuid import uuid1
+
+
+class Transaction:
+    pass
+
+
+class BankAccount:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
