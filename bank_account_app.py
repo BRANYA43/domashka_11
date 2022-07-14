da63c926b8a74a1b52f98d3d953a47568bb04d8f
Timchenko <tbd2504@gmail.com>
2022-07-14 19:45:06 +0300

    refactoring code

--- bank_account_app.py
+++ bank_account_app.py
@@ -23,20 +23,21 @@ from datetime import datetime
 
 class Transaction:
     """Клас транзакцій"""
+    COMMISION = '0.01'
 
     def __init__(self, amount: Decimal, _type: str, status='Settled'):
         """Ініціалізює кількість коштів, тип, статут, комісію за операцію та дату."""
         self.amount = round(amount, 2)
         self.type = _type
         self.status = status
-        self.commission = round(amount * Decimal(0.01), 2)
+        self.commission_calculated = round(amount * Decimal(self.COMMISION), 2)
         self.dt = datetime.now()
 
     def print_info(self, index):
         """Видає інформацію у консоль об транзакції. Номер, дату, статус, тип, кошти, комісію."""
         print(f'    {index}. {self.dt.strftime("%d.%m.%Y %H:%M:%S")}\n'
               f'        Status: {self.status}, Type: {self.type}\n'
-              f'        Sum: {self.amount}, Commission: {self.commission}')
+              f'        Sum: {self.amount}, Commission: {self.commission_calculated}')
         if self.status == 'Failed':
             print('        !!!Insufficient funds for transaction!!!')
 
@@ -56,8 +57,8 @@ class BankAccount:
         При поповненні рахунку комісія знімаеться з коштів що додаються до разунку.
         При виведенні коштів комісія знімаеться с рахунку користувача."""
         operations = {
-            'Deposit': self._balance + transaction.amount - transaction.commission,
-            'Withdrawal': self._balance - transaction.amount + transaction.commission
+            'Deposit': self._balance + transaction.amount - transaction.commission_calculated,
+            'Withdrawal': self._balance - transaction.amount + transaction.commission_calculated
         }
         self._balance = operations[transaction.type]
 
@@ -69,7 +70,7 @@ class BankAccount:
     def withdrawal(self, amount: str):
         """Виводить кошти з рахунку ствоюючи об'єкт транзакцій та зберігає у список транзакцій зі статусом Settled.
         Також перевіряє чи можливо зняти грощі, якщо ні то додає цю транзакію у список зі статусом Failed."""
-        if self._balance - Decimal(amount) < 0:
+        if self._balance - Decimal(amount) * Decimal(Transaction.COMMISION + '1') < 0:
             self._transactions.append(Transaction(Decimal(amount), 'Withdrawal', 'Failed'))
         else:
             self._transactions.append(Transaction(Decimal(amount), 'Withdrawal'))
