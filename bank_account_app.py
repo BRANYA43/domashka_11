0e057ca2fbc1ab885e355c636178e1ad6bcab391
Timchenko <tbd2504@gmail.com>
2022-07-14 19:22:28 +0300

    add doc string

--- bank_account_app.py
+++ bank_account_app.py
@@ -22,7 +22,10 @@ from datetime import datetime
 
 
 class Transaction:
+    """Клас транзакцій"""
+
     def __init__(self, amount: Decimal, _type: str, status='Settled'):
+        """Ініціалізює кількість коштів, тип, статут, комісію за операцію та дату."""
         self.amount = round(amount, 2)
         self.type = _type
         self.status = status
@@ -30,6 +33,7 @@ class Transaction:
         self.dt = datetime.now()
 
     def print_info(self, index):
+        """Видає інформацію у консоль об транзакції. Номер, дату, статус, тип, кошти, комісію."""
         print(f'    {index}. {self.dt.strftime("%d.%m.%Y %H:%M:%S")}\n'
               f'        Status: {self.status}, Type: {self.type}\n'
               f'        Sum: {self.amount}, Commission: {self.commission}')
@@ -38,13 +42,19 @@ class Transaction:
 
 
 class BankAccount:
+    """Клас банківського рахунку"""
+
     def __init__(self, name: str):
+        """Ініціалізює ім'я, ід, рахунок та список транзакцій."""
         self._name = name
         self._id = uuid1()
         self._balance = Decimal(0)
         self._transactions = []
 
     def calculate_balance(self, transaction):
+        """Розраховує кошти залежно від типу транзакції та враховує комісію.
+        При поповненні рахунку комісія знімаеться з коштів що додаються до разунку.
+        При виведенні коштів комісія знімаеться с рахунку користувача."""
         operations = {
             'Deposit': self._balance + transaction.amount - transaction.commission,
             'Withdrawal': self._balance - transaction.amount + transaction.commission
@@ -52,22 +62,26 @@ class BankAccount:
         self._balance = operations[transaction.type]
 
     def deposit(self, amount: str):
+        """Поповнює кошти на рахунок ствоюючи об'єкт транзакцій та зберігає у список транзакці зі статусом Settled."""
         self._transactions.append(Transaction(Decimal(amount), 'Deposit'))
         self.calculate_balance(self._transactions[-1])
 
     def withdrawal(self, amount: str):
+        """Виводить кошти з рахунку ствоюючи об'єкт транзакцій та зберігає у список транзакцій зі статусом Settled.
+        Також перевіряє чи можливо зняти грощі, якщо ні то додає цю транзакію у список зі статусом Failed."""
         if self._balance - Decimal(amount) < 0:
-            self._transactions.append(
-                Transaction(Decimal(amount), 'Withdrawal', 'Failed'))
+            self._transactions.append(Transaction(Decimal(amount), 'Withdrawal', 'Failed'))
         else:
             self._transactions.append(Transaction(Decimal(amount), 'Withdrawal'))
             self.calculate_balance(self._transactions[-1])
 
     @property
     def balance(self):
+        """Повертає рахунок користувача"""
         return self._balance
 
     def print_info(self):
+        """Видає інформацію об користувачі. Його ім'я, ід, рахунок та список останіх 10-ти транзакцій."""
         print(f'Name: {self._name}\n'
               f'ID: {self._id}\n'
               f'Balance: {self._balance}\n'
